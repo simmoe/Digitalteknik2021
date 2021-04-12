07d3dcf72e197d1f9e46bb56ec36784103862b12
simon moe <simmoe@gmail.com>
2021-04-12 16:23:16 +0200

    charts

--- p5-firebase-template/sketch.js
+++ p5-firebase-template/sketch.js
@@ -1,26 +1,41 @@
-let data 
+let data, chart
 
 function setup() {
-    //UNCOMMENT THIS TO GET DATA FROM FIREBASE
-    // db.collection("connection-name").doc("doc-name")
-    // .onSnapshot( doc => {
-    //     data = doc.data() 
-    // })
+//     UNCOMMENT THIS TO GET DATA FROM FIREBASE
+//     db.collection("collection-name").doc("doc-name")
+//     .onSnapshot( doc => {
+//         console.log(doc.data())
+//         showChart(doc.data())
+//     })
+
 }
 
-function showPieChart(data){
-    console.log(data)
+function showChart(data){
     chart && chart.destroy()
-    chart = new Chart(document.getElementById('chartCanvas'), {
-        type: 'doughnut',
+    chart = new Chart(document.getElementById('myChart'), {
+        type: 'bar',
         data: {
-            labels: data.labels,
+            labels: Object.keys(data),
             datasets: [{
-                data: data.values,
+                label: 'no-label',
+                data: Object.values(data),
                 backgroundColor: ['rgba(255, 99, 132, 0.2)','rgba(54, 162, 235, 0.2)','rgba(255, 206, 86, 0.2)','rgba(75, 192, 192, 0.2)'],
-                borderWidth: 3
-            }]
+                borderWidth: 3,
+                
+            }],
         },
+        options:{
+            plugins:{
+                legend:{
+                    display:false
+                },
+                title:{
+                    display:true,
+                    text:'chart title'
+                }
+            }
+        }
+
     });
 }
 
