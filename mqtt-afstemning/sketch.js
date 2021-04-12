0cd67d765d2902e99c2f5e42419d36d3b750bd3d
simon moe <simmoe@gmail.com>
2021-04-12 09:17:40 +0200

    minors

--- mqtt-afstemning/sketch.js
+++ mqtt-afstemning/sketch.js
@@ -7,7 +7,6 @@ let vote = {
 }
 //Vi har også en liste med farver til charts mm
 let backgroundColors = ['rgba(255, 99, 132, 0.2)','rgba(54, 162, 235, 0.2)','rgba(255, 206, 86, 0.2)','rgba(75, 192, 192, 0.2)']
-
 //og så skal vi bruge en timer
 let timer
 //og et antal sekunder det skal tage at svare
@@ -19,26 +18,32 @@ function setup() {
   mqttInit()
   //vi skal have besked hver gang der kommer nye beskeder i emnet "filmfestival-afstemning"
   client.subscribe('filmfestival-afstemning')
-  client.on('message', (topic, message)=>{
+  client.on('message', (topic, message) => {
     vote.votes[message.toString()] ++
     console.log(vote.votes)
   })
-  //vi løber mulighederne igennem og laver hver mulighed som en ny div
+  //Så skal vi oprette afstemningen - vi løber mulighederne igennem og laver hver mulighed som en ny div
   vote.options.map( (option, index) => {
     select('#options').child(createDiv(option).style('backgroundColor', backgroundColors[index]))
   })
-  //så starter vi tiden
+  //vi sætter antal sekunder ind i den div der viser tiden
   select('#timer').html(seconds)
+  //så starter vi tiden
   timer = setInterval(() => {
+    //hver gang der er gået et sekund, tæller vi sekunderne ned og viser den nye tid
     seconds--
     select('#timer').html(seconds)
+    //hvis alle sekunderne er gået, kalder vi funktionen showResults
     if (seconds == 0) showResults()
   }, 1000)
 }
 
 function showResults(){
+  //stop timeren
   clearInterval(timer)
+  //tilføj klassen 'show' til den div der viser vores chart
   select('#chart').addClass('show')
+  //opret chart med resultater fra afstemningen
   chart = new Chart(document.getElementById('chartCanvas'), {
       type: 'bar',
       data: {
