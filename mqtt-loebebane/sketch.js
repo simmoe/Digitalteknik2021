c1882b8a8989fa770e773d207ffd5fa7bcf57011
Simon <simmoe@gmail.com>
2021-04-30 16:09:05 +0200

    fixed loebebane to worked example

--- mqtt-loebebane/sketch.js
+++ mqtt-loebebane/sketch.js
@@ -1,21 +1,14 @@
 //client er den variabel der bruges til at oprette forbindelse til mqtt serveren
 let client 
-//Vi skal bruge en knap man kan trykke start på og en til at resette spillet
-let startButton, resetButton
 //vi skal bruge to variabler til at tage tid i millisekunder og til at gemme starttidspunktet  
 let milliseconds, seconds, startTime = 0; 
-//og en timer 
+//og en timer - som er en variabel i javascript, der kaldes i et interval 
 let timer; 
-//og et DIV element til at vise tiden
-let timerDiv 
 
 //Vi gemmer løberne som objekter i et array, hvor vi også har en reference til deres mqtt navn
-let runners = [ 
-                {'name':'løber 1', 'mqtt': 'm5-1', 'time':0 }, 
-                {'name':'løber 2', 'mqtt': 'm5-2', 'time':0 }, 
-              ]
-//og så skal vi bruge et DIV element til at vise løbernes tider, og et andet til at vise hvem der har vundet 
-let statsDiv, winnerDiv
+let runner = {'name':'løber 1', 'mqtt': 'm5-1', 'time':0 }
+//og laver en anden liste til highscores
+let highscore = [1.55, 1.89, 2.56, 5.55]
 
 //setup er den funktion der kører, før selve web-appen går starter 
 function setup() {
@@ -23,41 +16,30 @@ function setup() {
   mqttInit()
   //vi har ikke brug for et canvas eller en run funktion, selvom det er en del af p5.js
   noCanvas(); noLoop();
-  //vi subscriber til alle topics som starter med m5/
-  client.subscribe('m5/+')
-  //vi begynder meed at nulstille alle m5'ere
-  client.publish('m5/all', 'off')
-  //vi initialiserer startknappen
-  startButton = createButton('start') 
-  //når man trykker på knappen, skal spillet begynde - det sker i funktionen startGame
-  startButton.mouseReleased(startGame)
-  //og reset knappen
-  resetButton = createButton('nulstil')
-  //som kalder en funktion der nulstiller alt  
-  resetButton.mouseReleased(resetGame)
-  //vi initialiserer den DIV der skal vise tiden 
-  timerDiv = createDiv('0:00').class('timer')
-  //og de div'er der skal vise stats og vinder
-  statsDiv = createDiv().class('stats') 
-  winnerDiv = createDiv().class('winner')
+  //når der trykkes på knapperne
+  select('#readyButton').mouseReleased(()=>{goToPage(1)})
+  select('#startButton').mouseReleased(startGame)
+  select('#cancelButton').mouseReleased(resetGame)
+  select('#resetButton').mouseReleased(resetGame)
 
-  //vi bruger funktionen showStats til at vise løbernes tider
-  showStats()
-  //her modtager vi MQTT beskeder fra m5'erne 
+  //vi subscriber til beskeder fra m5/m5-1
+  client.subscribe('m5/m5-1')
+  //vi begynder med at nulstille m5'eren
+  client.publish('m5', 'off')
   client.on('message', (topic, message) => {
-    //så vi går de forskellige løbere igennem og ser hvem det er vi har fået besked fra
-    runners.map( runner => {
-      //når vi finder løberen tjekker vi at tiden i forvejen er 0 - og sætter så en sluttid 
-      if(topic.includes(runner.mqtt) && runner.time == 0) runner.time = seconds
-    }) 
-    //vi tjekker om alle løberes tider er sat 
-    let anyMoreRunners = runners.find( runner => runner.time == 0 )
-    //hvis de er det, kalder vi funktionen endGame()
-    if( ! anyMoreRunners ) endGame()
+    if(runner.time == 0) {
+      runner.time = seconds
+      endGame()
+    }
   }) 
 }
 
+function goToPage(where){
+  select('main').style('transform', 'translateX(-' + where * 100 + 'vw)')
+}
+
 function startGame(){
+  goToPage(2)
   //vi gemmer starttidspunktet i startTime - millisekunder siden 1970 
   startTime = new Date().getTime()
   //timeren sættes op som et interval, der hele tiden kalder funktionen showTime()
@@ -68,46 +50,37 @@ function showTime(){
   //vi sætter så milliseconds til antallet af millisekunder siden startTime
   milliseconds = new Date().getTime() - startTime 
   seconds = (milliseconds/1000).toFixed(2)
-  timerDiv.html( seconds )
-  showStats()
+  select('#timer').html( seconds )
 }
 
 //funktion der nulstiller spillet
 function resetGame(){
-  //først sætter vi alle løberes tider til 0
-  runners.map(runner => runner.time = 0)
-  //vi nulstiller timeren, og sætter de andre variabler og tekster tilbage til 0
-  window.clearInterval(timer)
-  milliseconds = 0
-  timerDiv.html('0:00')
-  winnerDiv.html('').removeClass('in')
-  showStats()
-} 
-
-//funktion der viser statistik om løbernes tider 
-function showStats(){
-  //Vi bygger en lille html-tekst, som viser løbernes tider - først en overskrift
-  stats = '<h3>Stats</h3>'
-  //så går vi løberne igennem og tilføjer deres navn og tid 
-  runners.map(runner => {
-    stats += runner.name + ': ' + runner.time + '</br>'
-  })
-  //til sidst viser vi hele teksten i statsDiv
-  statsDiv.html(stats)
-}
-
-function endGame(){
   //Stop tiden
   clearInterval(timer)
-  //vis stats en sidste gang
-  showStats()
-  //Vi skal finde ud af hvem der har vundet - først sætter vi en variabel til at pege på den allerførste løber  
-  let winner = runners[0]
-  //så løber vi alle løbere igennem og hvis deres tid er bedre end den første, sætter vi dem som vinder i stedet
-  runners.map( runner => runner.time < winner.time ? winner = runner : '' )
-  //og så kan vi vise hvem der har vundet
-  winnerDiv.html('<h3>' + winner.name + ' vinder med tiden: ' + winner.time +  '</h3>')
-  winnerDiv.addClass('in')
+  //sæt løberens tid til 0
+  runner.time = 0
+  //gå tilbage til forsiden
+  goToPage(0)
+} 
+
+
+function endGame(){
+  runner.time = seconds
+  //Stop tiden
+  clearInterval(timer)
+  select('#endTime').html(runner.time)
+  highscore.push(float(runner.time))
+  highscore.sort()
+  console.log(highscore)
+
+  select('#highscore').html('<h3>Highscore</h3>')
+  highscore.map( score => {
+    let div = createDiv(score)
+    if(score == runner.time) div.class('me')
+    select('#highscore').child(div)
+  })
+  //gå til slutsiden
+  goToPage(3)
 }
 
 
